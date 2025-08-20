a495db57786311f5082a782292aac62faf738679
yeswadams <yeswaadams73@gmail.com>
2025-08-20 23:48:16 +0300

    update the code and its file structure set up the routing and layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+// import supabase from '@/supabase-client'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/sign-in" element={<SigninForm/>} />
+        <Route path="/sign-up" element={<SignupForm/>} />
+       
+        {/* Private Rutes */}
+
+        <Route index element ={<Home />}/>
+      </Routes>
+    </main>
   )
 }
-
-export default App
+export default App;
